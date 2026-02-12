b7741f6ed31ce90a139348c5db7b148e5dfcc984
Ahmad Farhan QF <farhanhmd11@gmail.com@gmail.com>
2026-02-12 20:01:46 +0700

    Add .gitignore and video_stats.py

---
+++ video_stats.py
@@ -0,0 +1,33 @@
+import requests 
+import json
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env')
+
+API_KEY = os.getenv('API_KEY') 
+CHANNEL_NAME = 'MrBeast'
+
+def get_playlist_id():
+
+    try:
+        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_NAME}&key={API_KEY}'
+
+        response = requests.get(url)
+        # print(response)
+
+        data = response.json()
+        # print(json.dumps(data, indent=4))
+
+        channel_items = data["items"][0]
+        channel_playlist_Id = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
+        print(channel_playlist_Id)
+
+        return channel_playlist_Id
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    get_playlist_id()
